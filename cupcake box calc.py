a8b8e93f89046174c7cc4f5a87612dee3a11fdd9
Raghu17012009 <raghu01172009@gmail.com>
2026-08-22 19:22:37 +0530

    Refactor loop structure for cupcake box calculation

--- cupcake box calc.py
+++ cupcake box calc.py
@@ -1,21 +1,21 @@
 import math
-def loop():
-    while True:
-        def calc(num):
-            box=num/6
-            if num/6 == math.floor(num/6):
-                box=int(num/6)
-                print("Number of boxes to be bought "+str(box))
-            else:
-                box=math.floor(num/6)+1
-                print("Number of Boxes which can be bought = "+str(box))
 
-        num=input("Enter the Number of Cupcakes needed ")
-        if num.lower()=="exit":
-            break
-        if int(num)<6:
-            calc(int(num))
+while True:
+    def calc(num):
+        box=num/6
+        if num/6 == math.floor(num/6):
+            box=int(num/6)
+            print("Number of boxes to be bought "+str(box))
         else:
-            calc(int(num))
-        print(".......................................................")
-loop()
+            box=math.floor(num/6)+1
+            print("Number of Boxes which can be bought = "+str(box))
+
+    num=input("Enter the Number of Cupcakes needed ")
+    if num.lower()=="exit":
+        break
+    if int(num)<6:
+        calc(int(num))
+    else:
+        calc(int(num))
+    print(".......................................................")
+   
